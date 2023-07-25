cb442b88b38e3c4358e362bb8c4728ba0395334c
FIRST_NAME LAST_NAME <ublackhose@gmail.com>
2023-07-25 11:22:20 +0700

    start

--- test_2.php
+++ test_2.php
@@ -1,8 +1,55 @@
-<?
-require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+<?php
 
 
 
+function getSoapDilivery($ru_name_city = null, $weight = 1.0, $cost = 1.0, $volume = 1.0)
+{
+    if ($ru_name_city) {
+        $client = new SoapClient('https://ltl-ws.major-express.ru/ltl.asmx?WSDL');
+        $citys = json_decode(json_encode($client->dict_Cities()), true);
+        $city_to = null;
+        foreach ($citys['dict_CitiesResult']['enc_value']['City'] as $item) {
+            if ($item['Name'] == $ru_name_city) {
+                $item['Name'] = trim(preg_replace('/\s*\(.*?\)/', '', $item['Name']));
+                $city_to = $item;
+            }
+        }
 
-require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");
+
+        if ($city_to) {
+            $calc = json_decode(
+                json_encode(
+                    $client->CalculatorByWB(
+                        [
+                            "ShipperCityID" => 129,
+                            "ConsigneeCityID" => $city_to['Code'],
+                            "Weight" => $weight,
+                            "Volume"=> $volume,
+                            "Cost" => $cost
+                        ]
+                    )
+                ),
+                true
+            );
+
+
+            if ($calc['CalculatorByWBResult']['enc_value']['LTL_Calculator']) {
+                $calc = $calc['CalculatorByWBResult']['enc_value']['LTL_Calculator'];
+                return $calc;
+            }else{
+                return null;
+            }
+        } else {
+            return null;
+        }
+
+
+    }
+    return null;
+}
+
+echo "<pre>";
+print_r(getSoapDilivery("Новосибирск"));
+echo "</pre>";
+
 ?>
