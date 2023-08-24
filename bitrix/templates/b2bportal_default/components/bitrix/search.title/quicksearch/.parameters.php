b436ee78f4ee7a2278c0606ceeb1d984f1962eb0
FIRST_NAME LAST_NAME <ublackhose@gmail.com>
2023-08-24 17:58:07 +0700

    edit search

---
+++ bitrix/templates/b2bportal_default/components/bitrix/search.title/quicksearch/.parameters.php
@@ -0,0 +1,86 @@
+<?php
+
+use Bitrix\Main\Loader;
+use Bitrix\Main\Localization\Loc;
+
+/**
+ * @var array $arTemplateParameters
+ * @var array $arCurrentValues
+ * @var string $templateFolder
+ */
+
+if(!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true)
+    die();
+
+
+$priceGroups = [];
+if (Loader::includeModule('catalog'))
+{
+    $priceGroupsIterator = \Bitrix\Catalog\GroupTable::getList([
+        'order' => ['SORT' => 'ASC'],
+        'select' => ['ID', 'NAME','PROPERTY_CML2_ARTICLE' , 'TITLE' => 'CURRENT_LANG.NAME']
+    ]);
+    while ($group = $priceGroupsIterator->fetch())
+    {
+        $priceGroups[$group['NAME']] = '[' . $group['NAME'] . '] ' . $group['TITLE'];
+    }
+}
+
+$arTemplateParameters['SHOW_INPUT'] = [
+    'NAME' => Loc::getMessage('TP_BST_SHOW_INPUT'),
+    'TYPE' => 'CHECKBOX',
+    'DEFAULT' => 'Y',
+    'REFRESH' => 'Y',
+];
+
+$arTemplateParameters['INPUT_ID'] = [
+    'NAME' => Loc::getMessage('TP_BST_INPUT_ID'),
+    'TYPE' => 'STRING',
+    'DEFAULT' => 'title-search-input',
+];
+
+$arTemplateParameters['CONTAINER_ID'] = [
+    'NAME' => Loc::getMessage('TP_BST_CONTAINER_ID'),
+    'TYPE' => 'STRING',
+    'DEFAULT' => 'title-search',
+];
+
+$arTemplateParameters['PROP_CODE_ARTICLE'] = [
+    'NAME' => Loc::getMessage('TP_BST_PROP_CODE_ARTICLE'),
+    'TYPE' => 'STRING',
+    'DEFAULT' => 'ARTNUMBER'
+];
+
+$arTemplateParameters['PRICE_CODE'] = [
+    'NAME' => Loc::getMessage('TP_BST_PRICE_CODE'),
+    'TYPE' => 'LIST',
+    'MULTIPLE' => 'Y',
+    'VALUES' => $priceGroups,
+    'ADDITIONAL_VALUES' => 'Y',
+];
+
+$arTemplateParameters['PRICE_VAT_INCLUDE'] = [
+    'NAME' => Loc::getMessage('TP_BST_PRICE_VAT_INCLUDE'),
+    'TYPE' => 'CHECKBOX',
+    'DEFAULT' => 'Y'
+];
+
+$arTemplateParameters['CONVERT_CURRENCY'] = [
+    'NAME' => Loc::getMessage('TP_BST_CONVERT_CURRENCY'),
+    'TYPE' => 'CHECKBOX',
+    'DEFAULT' => 'N',
+    'REFRESH' => 'Y',
+];
+
+if ($arCurrentValues['CONVERT_CURRENCY'] === 'Y' && Loader::includeModule('currency'))
+{
+    $currencies = \Bitrix\Currency\CurrencyManager::getCurrencyList();
+
+    $arTemplateParameters['CURRENCY_ID'] = [
+        'NAME' => Loc::getMessage('TP_BST_CURRENCY_ID'),
+        'TYPE' => 'LIST',
+        'VALUES' => $currencies,
+        'DEFAULT' => \Bitrix\Currency\CurrencyManager::getBaseCurrency(),
+        'ADDITIONAL_VALUES' => 'Y',
+    ];
+}
---
+++ bitrix/templates/b2bportal_default/components/bitrix/search.title/quicksearch/template.php
@@ -0,0 +1,47 @@
+<?php
+
+use Bitrix\Main\Localization\Loc;
+
+/**
+ * @var CMain $APPLICATION
+ * @var CBitrixComponentTemplate $this
+ * @var CBitrixComponent $component
+ * @var array $arParams
+ * @var array $arResult
+ * @var string $templateFolder
+ */
+
+if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true)
+	die();
+
+
+$this->setFrameMode(true);
+
+$this->addExternalJs($templateFolder . '/dist/component.js');
+
+$inputId = trim($arParams["~INPUT_ID"]);
+if(!mb_strlen($inputId))
+	$inputId = 'title-search-input';
+
+$jsParams = [
+	'action' => $arResult['FORM_ACTION'],
+	'ajaxUrl' => POST_FORM_ACTION_URI,
+	'minLength' => 3,
+	'inputId' => $inputId,
+	'inputName' => 'q'
+];
+
+?>
+<div id="<?=$arParams['CONTAINER_ID']?>"></div>
+
+<script>
+(function () {
+	<?php $messages = Loc::loadLanguageFile(__FILE__); ?>
+	BX.message(<?=\CUtil::PhpToJSObject($messages)?>);
+
+	new B2BPortal.Components.QuickSearch(
+		document.getElementById('<?=$arParams['CONTAINER_ID']?>'),
+		<?=\CUtil::PhpToJSObject($jsParams, false, false, true)?>
+	);
+}());
+</script>
